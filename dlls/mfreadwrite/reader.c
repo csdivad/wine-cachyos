c03f146fd60016c601358fdd7aede5a0ad1cae9d
Yuxuan Shui <yshui@codeweavers.com>
2026-06-17 15:56:54 +0100

    mfreadwrite: Make sure ASYNC_READ op can't start until ASYNC_SEEK finishes.
    The ASYNC_SEEK operation sets an SEEKING flag which other operations
    wait on, so they won't start when a seek is in progress. Problem is
    this flag is set a bit too late in
    source_reader_async_commands_callback_Invoke, so the following race
    condition is possible:
    
    - Application calls SetCurrentPosition, which queues the ASYNC_SEEK
      operation but does not set SEEKING.
    - Application calls ReadSample. Since the SEEKING flag hasn't been
      set, it proceeds without waiting and queues an ASYNC_READ operation.
    - ASYNC_SEEK operation starts its execution, but hasn't reached
      IMFMediaSource_Start yet.
    - ASYNC_READ operation starts its execution, and calls
      IMFMediaStream_RequestSample. Media source queues a request sample
      job. reader->streams[i]->requests++. ASYNC_READ finishes.
    - ASYNC_SEEK operation calls IMFMediaSource_Start, the request sample
      job queued by the media source gets dropped.
    
    At this point, there is no request sample job running, but the
    stream->requests counter is still positive. From this point on the source
    reader will not request anymore samples, future ReadSample will never
    complete.
    
    Setting the SEEKING flag early, so that source reader will always wait in
    ReadSample so this kind of ordering cannot happen.
    
    (cherry picked from commit 7edd71a8d76c33ef708c726e87a3f772e7008d80)

--- dlls/mfreadwrite/reader.c
+++ dlls/mfreadwrite/reader.c
@@ -1529,10 +1529,10 @@ static HRESULT WINAPI source_reader_async_commands_callback_Invoke(IMFAsyncCallb
         case SOURCE_READER_ASYNC_SEEK:
 
             EnterCriticalSection(&reader->cs);
-            if (SUCCEEDED(IMFMediaSource_Start(reader->source, reader->descriptor, &command->u.seek.format,
+            if (FAILED(IMFMediaSource_Start(reader->source, reader->descriptor, &command->u.seek.format,
                     &command->u.seek.position)))
             {
-                reader->flags |= SOURCE_READER_SEEKING;
+                reader->flags &= ~SOURCE_READER_SEEKING;
             }
             LeaveCriticalSection(&reader->cs);
 
@@ -2269,6 +2269,8 @@ static HRESULT WINAPI src_reader_SetCurrentPosition(IMFSourceReaderEx *iface, RE
 
     if (SUCCEEDED(hr))
     {
+        reader->flags |= SOURCE_READER_SEEKING;
+
         for (i = 0; i < reader->stream_count; ++i)
         {
             reader->streams[i].last_sample_ts = 0;
@@ -2289,7 +2291,6 @@ static HRESULT WINAPI src_reader_SetCurrentPosition(IMFSourceReaderEx *iface, RE
         {
             if (SUCCEEDED(IMFMediaSource_Start(reader->source, reader->descriptor, format, position)))
             {
-                reader->flags |= SOURCE_READER_SEEKING;
                 while (reader->flags & SOURCE_READER_SEEKING)
                 {
                     SleepConditionVariableCS(&reader->state_event, &reader->cs, INFINITE);
