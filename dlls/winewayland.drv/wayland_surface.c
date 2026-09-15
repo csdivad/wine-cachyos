wine-mirror/wine

winewayland.drv: Avoid ABBA deadlocks between win_data_mutex and user_mutex.
Make the lock acquisition order consistent: always acquire the user_mutex lock
on the user32 side first, and then the win_data_mutex on the wayland side.
Otherwise, when the CCTV Video main program starts, there is a nearly 40%
probability of a race deadlock between set_client_surface and update_surface_region
(which indirectly calls wayland_window_surface_flush).

Signed-off-by: Jiangyi Chen <chenjiangyi@uniontech.com>

dlls/winewayland.drv/wayland_surface.c
@@ -1182,12 +1182,13 @@ static void wayland_client_surface_update(struct client_surface *client)
     struct wayland_client_surface *surface = impl_from_client_surface(client);
     HWND hwnd = client->hwnd, toplevel = client->toplevel;
     struct wayland_win_data *data;
+    BOOL visible = FALSE;
 
     TRACE("%s\n", debugstr_client_surface(client));
-
+    if(toplevel) visible = NtUserIsWindowVisible(hwnd);
     if (!(data = wayland_win_data_get(hwnd))) return;
 
-    if (toplevel && NtUserIsWindowVisible(hwnd))
+    if (toplevel && visible)
         wayland_client_surface_attach(surface, toplevel, &client->monitor_rect);
     else
         wayland_client_surface_attach(surface, NULL, NULL);

csdivad/wine-cachyos

dlls/winewayland.drv/wayland_surface.c
@@ -1444,12 +1444,13 @@ static void wayland_client_surface_update(struct client_surface *client)
     struct wayland_client_surface *surface = impl_from_client_surface(client);
     HWND hwnd = client->hwnd, toplevel = NtUserGetAncestor(hwnd, GA_ROOT);
     struct wayland_win_data *data;
+    BOOL visible = FALSE;
 
     TRACE("%s\n", debugstr_client_surface(client));
-
+    if (toplevel) visible = is_client_visible(hwnd);
     if (!(data = wayland_win_data_get(hwnd))) return;
 
-    if (toplevel && is_client_visible(hwnd))
+    if (toplevel && visible)
         wayland_client_surface_attach(surface, toplevel);
     else
         wayland_client_surface_attach(surface, NULL);
@@ -1575,10 +1576,12 @@ void set_client_surface(HWND hwnd, struct wayland_client_surface *new_client)
     HWND toplevel = NtUserGetAncestor(hwnd, GA_ROOT);
     struct wayland_client_surface *old_client;
     struct wayland_win_data *data;
+    BOOL visible = FALSE;
 
     /* ownership is shared with the callers, the last caller to release
      * its reference will also destroy it and clear our pointer. */
 
+    if (toplevel) visible = is_client_visible(hwnd);
     if (!(data = wayland_win_data_get(hwnd))) return;
 
     if (new_client != data->client_surface)
@@ -1588,7 +1591,7 @@ void set_client_surface(HWND hwnd, struct wayland_client_surface *new_client)
 
         if ((data->client_surface = new_client))
         {
-            if (toplevel && is_client_visible(hwnd))
+            if (toplevel && visible)
                 wayland_client_surface_attach(new_client, toplevel);
             else
                 wayland_client_surface_attach(new_client, NULL);