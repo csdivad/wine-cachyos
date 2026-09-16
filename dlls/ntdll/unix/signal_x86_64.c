wine-mirror/wine

ntdll: Fix some SIMD exception codes.
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=56213

dlls/ntdll/unix/signal_x86_64.c
@@ -2459,7 +2459,25 @@ static void fpe_handler( int signal, siginfo_t *siginfo, void *_sigcontext )
         rec.NumberParameters = 2;
         rec.ExceptionInformation[0] = 0;
         rec.ExceptionInformation[1] = context.c.FltSave.MxCsr;
-        if (CS_sig(sigcontext) != cs64_sel) rec.ExceptionCode = STATUS_FLOAT_MULTIPLE_TRAPS;
+        if (CS_sig(sigcontext) != cs64_sel)
+        {
+            switch (siginfo->si_code)
+            {
+            case FPE_FLTDIV:
+            case FPE_FLTINV:
+                rec.ExceptionCode = STATUS_FLOAT_MULTIPLE_TRAPS;
+                break;
+            case FPE_FLTOVF:
+            case FPE_FLTUND:
+            case FPE_FLTRES:
+                rec.ExceptionCode = STATUS_FLOAT_MULTIPLE_FAULTS;
+                break;
+            default:
+                FIXME("unknown SIMD exception: %#x\n", siginfo->si_code);
+                rec.ExceptionCode = STATUS_FLOAT_MULTIPLE_TRAPS;
+                break;
+            }
+        }
     }
     setup_raise_exception( data, sigcontext, &rec, &context );
 }

csdivad/wine-cachyos

Adapted: the x86_64 half is ported as well. The earlier revision of this port
carried only the i386 half, on the reading that this lineage's
signal_x86_64.c fpe_handler() had "no TRAP_x86_CACHEFLT SIMD branch" and so
offered no anchor. It does have that branch - in upstream's pre-fix form
(always STATUS_FLOAT_MULTIPLE_TRAPS in 32-bit code on x86_64). The hunk only
failed to apply because this lineage still names the context variable
`ucontext` where the upstream pre-image says `sigcontext` (upstream has since
refactored the function to take `_sigcontext` plus a local `sigcontext`, and
to thread a thread_data pointer through init_handler/save_context). The switch
is therefore applied with that one name substituted for the local variable;
the resulting block is byte-identical to upstream's post-image after
normalising `sigcontext`/`ucontext`.
Compile-verified: dlls/ntdll/ntdll.so links clean with the patch in the
configured tree (no new warnings).
(cherry picked from commit 209a01f4e7b96244856f39414b49bcad674c8f2e)

dlls/ntdll/unix/signal_x86_64.c
@@ -2875,7 +2875,25 @@ static void fpe_handler( int signal, siginfo_t *siginfo, void *sigcontext )
         rec.NumberParameters = 2;
         rec.ExceptionInformation[0] = 0;
         rec.ExceptionInformation[1] = context.c.FltSave.MxCsr;
-        if (CS_sig(ucontext) != cs64_sel) rec.ExceptionCode = STATUS_FLOAT_MULTIPLE_TRAPS;
+        if (CS_sig(ucontext) != cs64_sel)
+        {
+            switch (siginfo->si_code)
+            {
+            case FPE_FLTDIV:
+            case FPE_FLTINV:
+                rec.ExceptionCode = STATUS_FLOAT_MULTIPLE_TRAPS;
+                break;
+            case FPE_FLTOVF:
+            case FPE_FLTUND:
+            case FPE_FLTRES:
+                rec.ExceptionCode = STATUS_FLOAT_MULTIPLE_FAULTS;
+                break;
+            default:
+                FIXME("unknown SIMD exception: %#x\n", siginfo->si_code);
+                rec.ExceptionCode = STATUS_FLOAT_MULTIPLE_TRAPS;
+                break;
+            }
+        }
     }
     setup_raise_exception( sigcontext, &rec, &context );
 }